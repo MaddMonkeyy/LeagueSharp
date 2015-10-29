9bcb1c77db1d5df00e2651bbc3cbe38e0030425a
nbinkg <nbinkg@gmail.com>
2015-10-29 18:22:29 +0700

    Update AssemblyInfo.cs

--- Apollo's Viktor VH/AssemblyInfo.cs
+++ Apollo's Viktor VH/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Viktor")]
+[assembly: AssemblyTitle("Viktor VH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Viktor")]
+[assembly: AssemblyProduct("Viktor VH")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
